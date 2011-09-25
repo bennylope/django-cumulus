1f252de871dd6a0217cc0ce1f613d9abf107d496
Ben Lopatin <ben.lopatin@wellfireinteractive.com>
2011-09-25 13:16:52 -0400

    Add cdn or local context processor

--- cumulus/context_processors.py
+++ cumulus/context_processors.py
@@ -13,3 +13,18 @@ def cdn_url(request):
     cdn_url = container_url + static_url
 
     return {'CDN_URL': cdn_url}
+
+
+def static_cdn_url(request):
+    """
+    Context processor that switches between full CDN URL in templates and a
+    locally definined static media URL. Ideal for local development.
+
+    Defaults to the opposite of the project's DEBUG status.
+    """
+    use_cdn = getattr(settings, 'CUMULUS_USE_CDN_STATIC', not(settings.DEBUG))
+    if use_cdn:
+        url = cdn_url(request)['CDN_URL']
+    else:
+        url = settings.STATIC_URL
+    return {'STATIC_CDN_URL': url}
